3ed47bd82b89c5d69c00a67546982c66980603be
Csbilal <mbcs7550@gmail.com>
2022-10-04 10:46:39 +0500

    index changes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { connectDB } from "./Config/MongoConfig.js";
-<<<<<<< HEAD
 import addProdutRoute from "./routes/addProductRoute.js";
 import cors from "cors";
+
+import router from "./routes/userRoute.js";
+
 const app = express();
 
 const port = process.env.PORT || 5000;
@@ -11,21 +13,6 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/products", addProdutRoute);
-=======
-import bodyParser from "body-parser";
-import cors from "cors";
-import router from "./routes/userRoute.js";
-import path from "path"
-// const userRoute = require("./routes/userRout");
-const app = express();
-
-const port = process.env.PORT || 5000;
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-const _dirname = path.resolve();
-app.use("uploads", express.static(path.join(_dirname, "uploads")));
->>>>>>> 4efeec53471d92b640489366e49b66274c8d7284
 
 app.use("/api/users/", router);
 app.listen(port, (req, res) => {
